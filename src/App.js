7aae207b6c5933a27b09800e978b9ef18e236712
vasilymur <vmouravi@gmail.com>
2021-09-30 18:00:45 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="wrap">
+        <div className="card">
+          <div className="card-image">
+            <img src="./sea.jpg" alt="surfing"/>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
